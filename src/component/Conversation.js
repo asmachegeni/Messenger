f8bb3b21b5e2c55bca480a67b3b2185abd553272
asma <as.ch.2280@gmail.com>
2022-11-19 00:45:27 +0330

    bug fix in conversation component for show messages

--- src/component/Conversation.js
+++ src/component/Conversation.js
@@ -5,9 +5,9 @@ import "./../style/Conversation.css";
 const Conversation = ({ conversationInfo, userPhone }) => {
   return (
     <div className="ConversationContainer">
-      <Title name={conversationInfo[0].name}/>
+      <Title name={conversationInfo[0].name} />
       <div className="contentConversation">
-        {conversationInfo.map((message) => (
+        {conversationInfo[0].messages.map((message) => (
           <Message
             text={message.text}
             date={message.date}
