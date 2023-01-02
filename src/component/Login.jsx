9461a9f6a6f420a240543ba06de1df251bf9bd53
asma <as.ch.2280@gmail.com>
2023-01-03 00:51:55 +0330

    add fetch

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./../style/Logins.css";
 import User from "./../assets/user1.png";
 import { AiOutlineIdcard } from "react-icons/ai";
 import { TiLockClosed, TiLockOpen } from "react-icons/ti";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Cookies from "js-cookie";
 const Login = () => {
   const Navigate = useNavigate();
   const [username, setUsername] = useState("");
@@ -11,12 +13,51 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showPassError, setShowPassError] = useState(false);
   const [showusernameError, setShowusernameError] = useState(false);
-  const handleForm = () => {
-    //http requset
-    // if succesfull go next page
-    // else show error message
-    Navigate("/ChatRoom");
+  const fe = async () => {
+    await fetch("http://asmachegeni.ir/sanctum/csrf-cookie", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {});
   };
+  useEffect(() => {
+    // fe();
+    axios
+      .get("http://asmachegeni.ir/sanctum/csrf-cookie", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+      .then(function (response) {
+        console.log(response.headers.toString());
+      });
+    // console.log(Cookies.get("XSRF-TOKEN"));
+    console.log(document.cookie);
+    // fetch("http://asmachegeni.ir/api/register", {
+    //       method: "POST",
+    //       headers: {
+    //         "X-XSRF-TOKEN": token,
+    //         "Accept": "application/json",
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify({
+    //         username: "asma",
+    //         password: 1053522,
+    //         name: "asma",
+    //       }),
+    //     })
+    //       .then((response) => {
+    //         response.json();
+    //       })
+    //       .then((res) => {
+    //         console.log(res);
+    //       });
+  }, []);
+  const handleForm = () => {};
+
   const IconClick = () => {
     setShowPassword(!showPassword);
   };
